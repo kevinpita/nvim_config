0f5e0efe4de425690b780c6a23a134b264852419
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2021-07-08 15:52:17 +0200

    shorten_path removed, no longer valid

--- lua/plugins/configs/telescope/init.lua
+++ lua/plugins/configs/telescope/init.lua
@@ -20,7 +20,6 @@ require'telescope'.setup {
         file_sorter = require'telescope.sorters'.get_fuzzy_file,
         file_ignore_patterns = {"venv"},
         generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
-        shorten_path = true,
         winblend = 0,
         border = {},
         borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
