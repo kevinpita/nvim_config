5e20af96b50dee19e0f1baf35de4bf8fac2efed6
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2021-07-13 10:40:02 +0200

    java test file

---
+++ tests/A.java
@@ -0,0 +1,17 @@
+package tests;
+
+public class A{
+
+    private String a = "abc";
+
+    public static void main(String[] args){
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
